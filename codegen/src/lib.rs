1f3b124bb7079b685c493f31798149f31a9108e4
Michael Mitteregger <michael.mitteregger@gmx.at>
2019-08-16 16:40:08 +0200

    Fix unused attribute warning by removing the attribute in the generated code

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -5,8 +5,9 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 use proc_macro2::Span;
-use quote::quote;
 use syn::{Error, FnArg};
+
+use quote::quote;
 
 #[proc_macro_attribute]
 pub fn test_attr(_args: TokenStream, input: TokenStream) -> TokenStream {
@@ -14,7 +15,7 @@ pub fn test_attr(_args: TokenStream, input: TokenStream) -> TokenStream {
         .map_err(|error| error.span().unstable().error(error.to_string()))
         .map_err(|d| d.help(String::from("#[test_attr] can only be used on functions")));
 
-    let function = match function_result {
+    let mut function = match function_result {
         Ok(function) => function,
         Err(diag) => {
             diag.emit();
@@ -22,43 +23,48 @@ pub fn test_attr(_args: TokenStream, input: TokenStream) -> TokenStream {
         }
     };
 
-    match function.sig.inputs.first() {
-        Some(first_fn_arg) => {
-            match first_fn_arg {
-                FnArg::Receiver(_receiver) => {
-                    emit_error(
-                        "first #[test_attr] function argument should not be any variant of `self`",
-                    );
-                    TokenStream::new()
-                }
-                FnArg::Typed(pat_type) => {
-                    match pat_type.attrs.first() {
-                        Some(first_fn_arg_attr) => {
-                            if first_fn_arg_attr.path.is_ident("foo") {
-                                let token_stream = quote! {
-                                    // this does not make much sense,
-                                    // it's only an simple example for code generation
-                                    // depending on function argument attributes
-                                    #function
-                                };
-                                token_stream.into()
-                            } else {
-                                emit_error("first #[test_attr] function argument should annotated with `#[foo]`");
-                                TokenStream::new()
-                            }
-                        }
-                        None => {
-                            emit_error("first #[test_attr] function argument should annotated with `#[foo]`");
-                            TokenStream::new()
-                        }
-                    }
-                }
+    let mut function_sig_args_iter = function.sig.inputs.iter_mut();
+    let arg_with_foo_attr_present = match function_sig_args_iter.next() {
+        Some(first_fn_arg) => match first_fn_arg {
+            FnArg::Receiver(_receiver) => {
+                emit_error(
+                    "first #[test_attr] function argument should not be any variant of `self`",
+                );
+                return TokenStream::new();
             }
-        }
+            FnArg::Typed(pat_type) => {
+                let mut arg_with_foo_attr_present = false;
+
+                pat_type.attrs.retain(|fn_arg_attr| {
+                    if fn_arg_attr.path.is_ident("foo") {
+                        arg_with_foo_attr_present = true;
+                        false
+                    } else {
+                        true
+                    }
+                });
+
+                arg_with_foo_attr_present
+            }
+        },
         None => {
             emit_error("#[test_attr] function needs at least one argument");
-            TokenStream::new()
+            return TokenStream::new();
         }
+    };
+    drop(function_sig_args_iter);
+
+    if arg_with_foo_attr_present {
+        let token_stream = quote! {
+            // this does not make much sense,
+            // it's only an simple example for code generation
+            // depending on function argument attributes
+            #function
+        };
+        token_stream.into()
+    } else {
+        emit_error("first #[test_attr] function argument should annotated with `#[foo]`");
+        TokenStream::new()
     }
 }
 
